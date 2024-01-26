f1206e89fd8ef2f98c3516fc544a01dc8a72d79e
Bryan Pepe <bryanpepe@Bryans-MacBook-Pro.local>
2024-01-26 00:02:48 -0500

    Function created to crawl pages recursively.

--- crawl.js
+++ crawl.js
@@ -1,28 +1,51 @@
 const { link } = require("fs");
 const { url } = require("inspector");
 const { JSDOM } = require("jsdom");
+const { normalize } = require("path");
 
-async function crawlPage(urlString) {
-  console.log(`crawling ${urlString}`);
+async function crawlPage(baseURL, currentURL, pagesCrawled) {
+  const currentURLObj = new URL(currentURL);
+  const baseURLObj = new URL(baseURL);
+
+  if (currentURLObj.hostname !== baseURLObj.hostname) {
+    return pagesCrawled;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+
+  if (pagesCrawled[normalizedCurrentURL] > 0) {
+    pagesCrawled[normalizedCurrentURL]++;
+    return pagesCrawled;
+  }
+
+  pagesCrawled[normalizedCurrentURL] = 1;
+
+  console.log(`crawling ${currentURL}`);
+  let htmlBody = "";
   try {
-    const response = await fetch(urlString);
+    const response = await fetch(currentURL);
     if (response.status > 399) {
       console.log(
-        `error in fetch with status code ${response.status} on page ${urlString}`
+        `error in fetch with status code ${response.status} on page ${currentURL}`
       );
-      return;
+      return pagesCrawled;
     }
     const contentType = response.headers.get("content-type");
     if (!contentType.includes("text/html")) {
       console.log(
-        `skipping non-html page ${urlString}, content type: ${contentType}`
+        `skipping non-html page ${currentURL}, content type: ${contentType}`
       );
-      return;
+      return pagesCrawled;
     }
-    console.log(await response.text());
+    htmlBody = await response.text();
   } catch (err) {
-    console.log(`error crawling ${urlString}: ${err.message}`);
+    console.log(`error crawling ${currentURL}: ${err.message}`);
   }
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+  for (const nextURL of nextURLs) {
+    pagesCrawled = await crawlPage(baseURL, nextURL, pagesCrawled);
+  }
+  return pagesCrawled;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -33,7 +56,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
     if (linkElement.href.slice(0, 1) === "/") {
       try {
         const urlObj = new URL(`${baseURL.toLowerCase()}${linkElement.href}`);
-        urls.push(`${baseURL.toLowerCase()}${linkElement.href}`);
+        urls.push(urlObj.href);
       } catch (err) {
         console.log(`error with relative url: ${err.message}`);
       }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require("./crawl");
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("No website provided");
     process.exit(1);
@@ -9,9 +9,13 @@ function main() {
     console.log("Too many arguments");
     process.exit(1);
   }
-  const url = process.argv[2];
-  console.log("starting crawl of " + url);
-  crawlPage(url);
+  const baseURL = process.argv[2];
+  console.log("starting crawl of " + baseURL);
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
